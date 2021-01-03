4410fa397734881d02016536aed37c83c6a4649b
kingofraccoons <zvezdakov2015@yandex.ru>
2021-01-03 22:59:02 +0700

    WORK GRAPH!!!!!!! WOW!!

--- app/src/main/java/com/example/graphguilibrary/MainActivity.kt
+++ app/src/main/java/com/example/graphguilibrary/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.example.graphguilibrary
 
-import android.graphics.Canvas
-import android.graphics.PointF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.Toast
-import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
-//import com.example.graphguilibrary.testModel.TestModel
-import com.example.graphguilibrary.testModel.TestNiceCreatorGraph
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
